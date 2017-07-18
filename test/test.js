724955e5db9425406cf07fb8d9d49bf8720dfc8f
Matthias Rolke <mr.amtrack@gmail.com>
2017-07-18 22:57:52 +0200

    run force-dev-tool in test using npx

--- test/test.js
+++ test/test.js
@@ -4,14 +4,14 @@ var child = require('child_process');
 describe('Retrieving a PermissionSet', function() {
   this.timeout(10 * 60 * 1000);
   before(function() {
-    assert.equal(child.spawnSync('force-dev-tool', ['deploy', '-d', 'test/unpackaged/setup'], {}).status, 0);
+    assert.equal(child.spawnSync('npx', ['force-dev-tool', 'deploy', '-d', 'test/unpackaged/setup'], {}).status, 0);
   });
   after(function() {
-    assert.equal(child.spawnSync('force-dev-tool', ['deploy', '-d', 'test/unpackaged/teardown'], {}).status, 0);
+    assert.equal(child.spawnSync('npx', ['force-dev-tool', 'deploy', '-d', 'test/unpackaged/teardown'], {}).status, 0);
   });
   describe('standalone', function() {
     it('should finish without errors', function() {
-      assert.equal(child.spawnSync('force-dev-tool', ['retrieve', '-d', 'test/fixtures/permissionset-only'], {}).status, 0);
+      assert.equal(child.spawnSync('npx', ['force-dev-tool', 'retrieve', '-d', 'test/fixtures/permissionset-only'], {}).status, 0);
     });
     it('should only return entries associated with the PermissionSet', function() {
       var result = child.spawnSync('git', ['diff', 'test/fixtures/permissionset-only/permissionsets/Test.permissionset'], {});
@@ -20,7 +20,7 @@ describe('Retrieving a PermissionSet', function() {
   });
   describe('with CustomFields', function() {
     it('should finish without errors', function() {
-      assert.equal(child.spawnSync('force-dev-tool', ['retrieve', '-d', 'test/fixtures/permissionset-with-customfields'], {}).status, 0);
+      assert.equal(child.spawnSync('npx', ['force-dev-tool', 'retrieve', '-d', 'test/fixtures/permissionset-with-customfields'], {}).status, 0);
     });
     it('should only return entries associated with the PermissionSet', function() {
       var result = child.spawnSync('git', ['diff', 'test/fixtures/permissionset-with-customfields/permissionsets/Test.permissionset'], {});
@@ -29,7 +29,7 @@ describe('Retrieving a PermissionSet', function() {
   });
   describe('with a CustomObject', function() {
     it('should finish without errors', function() {
-      assert.equal(child.spawnSync('force-dev-tool', ['retrieve', '-d', 'test/fixtures/permissionset-with-customobject'], {}).status, 0);
+      assert.equal(child.spawnSync('npx', ['force-dev-tool', 'retrieve', '-d', 'test/fixtures/permissionset-with-customobject'], {}).status, 0);
     });
     it('should only return entries associated with the PermissionSet', function() {
       var result = child.spawnSync('git', ['diff', 'test/fixtures/permissionset-with-customobject/permissionsets/Test.permissionset'], {});
